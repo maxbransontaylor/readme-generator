4e664a460bf4aabcda1657c98f92f1f0b379be87
Max Taylor <maxbransontaylor@gmail.com>
2022-07-29 21:21:32 -0400

    succesfully asks for title and sends information to generateMarkdown

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  //{ type: "input", name: "", message: "" },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init().then((data) => generateMarkdown(data));
